f009e78d56094cfc9ee7edc0af007de3558fbf50
jobanpreet10 <147429245+jobanpreet10@users.noreply.github.com>
2025-06-07 23:13:29 +0530

    Create 3170.cpp

---
+++ 3170.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    typedef pair<char,int>P;
+    struct comp{
+        bool operator()(P &p1,P &p2){
+            if(p1.first == p2.first){
+                return p1.second < p2.second;
+            }
+            return p1.first > p2.first;
+        }
+    };
+    string clearStars(string s) {
+        string result;
+
+        int n = s.length();
+        priority_queue< P , vector<P> ,comp >pq;
+
+        for(int i= 0; i < n; i++){
+            if(s[i] != '*'){
+                pq.push({s[i],i});
+            }
+            else{
+                int idx = pq.top().second;
+                pq.pop();
+                s[idx] = '*';
+            }
+        }
+        for(int i  =0 ;i < n ; i++){
+            if(s[i] != '*'){
+                result += s[i];
+            }
+        }
+
+        return result;
+    }
+};
